0f7a917b73d10b24c6248c7b2faa7bbd13d34b9e
Marco Santiago <barzha001@gmail.com>
2023-12-21 18:46:12 -0600

    terminado

---
+++ main.js
@@ -0,0 +1,14 @@
+//Actividad 1
+const numeros = Array.from ({length:10}, ()=> Math.floor(Math.random()*1000))
+console.log(numeros)
+//Actividad 2
+/*
+let usuario = prompt ('Ingresa el nombre de 5 paises de tu agrado, te sugiero que separes el nombre de cada pais comn una coma')
+let arreglo = usuario.split(',');
+console.log(usuario);*/
+//Actividad 3
+/*
+const arreglo =[10, 40, 30, 20, 15, 5]
+Math.min.apply(null, arreglo);
+Math.max.apply(null, arreglo);
+console.log(' El arreglo ordenado de menor a mayor, muestra el número menor ' + Math.min.apply(null, arreglo) + ' y el número mayor ' + Math.max.apply(null, arreglo))*/
